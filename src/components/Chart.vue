59b555ea3446cab82d50732b7b2cf30d74698554
inshalayaz <inshal.ayaz14@gmail.com>
2022-06-14 21:57:15 +0500

    charts

---
+++ src/components/Chart.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <!-- <Bar v-if="type === 'bar'" :chart-data="chartData" :height="height" /> -->
+        <!-- <LineC v-if="type === 'line'" :chart-data="chartData" :height="height" /> -->
+        <!-- <Doughnut v-if="type === 'doughnut'" :chart-data="chartData" :height="height" /> -->
+
+        <v-component :is="type" :chart-data="chartData" :height="height"></v-component>
+    </div>
+    
+</template>
+
+
+<script>
+import { Bar, Line as LineC, Doughnut } from 'vue-chartjs/legacy'
+import Chart from 'chart.js/auto';
+
+export default {
+    name: 'Chart',
+    components: {
+        bar: Bar,
+        lineC: LineC, 
+        doughnut: Doughnut
+    },
+    props: ['chartData', 'type'],
+    data(){
+    return {
+      height: 100,
+    }
+  }
+}
+</script>
+
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container class="home">
+  <v-row>
+    <v-col class="col-md-12">
+      <v-btn @click="handleClick(0)">
+        <v-icon > {{ barIcon }} </v-icon>
+      </v-btn>
+      <v-btn @click="handleClick(1)">
+        <v-icon> {{ lineIcon }} </v-icon>
+      </v-btn>
+      <v-btn @click="handleClick(2)">
+        <v-icon> {{ donutIcon }} </v-icon>
+      </v-btn>
+    </v-col>
+  </v-row>
+
+    <Chart :chartData="chartData" :type="type"></Chart>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Chart from '../components/Chart.vue';
+import { mdiChartBar, mdiChartLine, mdiChartDonut } from '@mdi/js';
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Chart
+  },
+  data(){
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March', 'April', 'May'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20, 12, 80, 46]
+          }
+        ]
+      },
+      barIcon: mdiChartBar,
+      lineIcon: mdiChartLine,
+      donutIcon: mdiChartDonut,
+      type: 'bar'
+    }
+  },
+  methods: {
+    handleClick(id){
+      if(id === 0){
+        this.type = 'bar'
+      }else if(id === 1){
+        this.type = 'lineC'
+      }else if(id === 2){
+        this.type = 'doughnut'
+      }
+
+// enum = {
+//   0: () => { this.type = 'bar'}
+// }
+      // enum[id]?.()
+    }
   }
 };
 </script>
